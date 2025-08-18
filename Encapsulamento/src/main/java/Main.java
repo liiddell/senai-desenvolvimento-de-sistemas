a604bc8edfe540a67f852e8068e89d0993917003
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-08-18 10:44:02 -0300

    Adição de cliente e produto na main

--- Encapsulamento/src/main/java/Main.java
+++ Encapsulamento/src/main/java/Main.java
@@ -3,9 +3,13 @@ public class Main {
 
         Cliente c1 = new Cliente("Jorge", 30);
         c1.exibirDados();
-
-        System.out.println("-------------------");
         Produto p1 = new Produto("Notebook", 3500.00);
         p1.exibirDados();
+        System.out.println("-------------------");
+        Cliente c2 = new Cliente("Ana", 25);
+        c2.exibirDados();
+        Produto p2 = new Produto("Smartphone", 1500.00);
+        p2.exibirDados();
+        System.out.println("-------------------");
     }
 }
