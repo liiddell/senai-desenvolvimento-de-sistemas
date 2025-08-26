a1bdc1d41195d23ae16498810dbf795a94646253
Victor Hugo <hugo16domingos@outlook.com>
2025-08-26 09:14:26 -0300

    Atividade nova

---
+++ RelacionamentoClasses/src/main/java/Pessoa.java
@@ -0,0 +1,92 @@
+public class Pessoa {
+    private static Integer contId = 0;
+    private Integer id;
+    private String nome;
+    private int idade;
+    private String telefone;
+    private String email;
+
+    private Sexo sexo;
+
+    private Endereco endereco;
+
+
+    public Pessoa(String nome, int idade, String telefone, String email, Sexo sexo, Endereco endereco) {
+        this.id = ++contId;
+        this.nome = nome;
+        this.idade = idade;
+        this.telefone = telefone;
+        this.email = email;
+        this.sexo = sexo;
+        this.endereco = endereco;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Sexo getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(Sexo sexo) {
+        this.sexo = sexo;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa:" +
+                "id= " + id +
+                "| nome: " +getNome() +
+                "| idade: " +getIdade() +
+                "| telefone: " +getTelefone() +
+                "| email: " +getEmail()+
+                "| sexo: " + getSexo()+
+                "| endereco: " +getEndereco() +
+                "\n";
+    }
+}
