474bcb1aa9f18968926c668819b5cb73470e613e
Victor Hugo <hugo16domingos@outlook.com>
2025-08-26 11:17:02 -0300

    Adição de um novo exercício

---
+++ Heranca/src/main/java/heranca03/Calca.java
@@ -0,0 +1,7 @@
+package heranca03;
+
+public class Calca extends Produto{
+    public Calca(String marca, String cor, String tamanho, double preco) {
+        super(marca, cor, tamanho, preco);
+    }
+}
