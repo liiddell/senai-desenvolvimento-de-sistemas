6efd57008eeede74bd0ea04202926d0e5bb85ea0
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-09-02 08:03:38 -0300

    Finalização

---
+++ Interface/src/main/java/org/polimorfismo/Galo.java
@@ -0,0 +1,9 @@
+package org.polimorfismo;
+
+public class Galo implements Animal {
+
+    @Override
+    public String emitirSom() {
+        return "Cocoricó!";
+    }
+}
--- Interface/src/main/java/org/polimorfismo/Main.java
+++ Interface/src/main/java/org/polimorfismo/Main.java
@@ -2,13 +2,25 @@ package org.polimorfismo;
 
 public class Main {
     public static void main(String[] args) {
-        Cachorro cachorro = new Cachorro();
 
+        System.out.println("Exemplos de Polimorfismo em Java");
+        Cachorro cachorro = new Cachorro();
         System.out.println("Som do cachorro:");
         System.out.println(cachorro.emitirSom());
+        System.out.println("-------------------");
 
         Gato gato = new Gato();
         System.out.println("Som do gato:");
         System.out.println(gato.emitirSom());
+
+        System.out.println("-------------------");
+        Galo galo = new Galo();
+        System.out.println("Som do galo:");
+        System.out.println(galo.emitirSom());
+
+        System.out.println("-------------------");
+        Pato pato = new Pato();
+        System.out.println("Som do pato:");
+        System.out.println(pato.emitirSom());
     }
 }
