976c300583bff2c9a5b633dca87ac58200bbf0ab
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-09-15 11:31:28 -0300

    Adição produto

---
+++ spring-boot/aula/src/main/java/com/example/aula/controller/ProdutoController.java
@@ -0,0 +1,35 @@
+package com.example.aula.controller;
+
+
+import com.example.aula.model.Produto;
+import com.example.aula.repository.ProdutoRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/produto")
+public class ProdutoController {
+    private ProdutoRepository produtoRepository;
+
+    public ProdutoController(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
+
+    @GetMapping
+    public List<Produto> listar() {
+        return produtoRepository.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<Produto> salvar(Produto produto) {
+        produtoRepository.save(produto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(produto);
+    }
+}
