d928c364ac0a1a0aa0d62e0b4282c2d8e36b333a
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-10-06 11:06:33 -0300

    Atualização

--- spring-boot/emprego/src/main/java/com/example/emprego/model/Funcionario.java
+++ spring-boot/emprego/src/main/java/com/example/emprego/model/Funcionario.java
@@ -1,11 +1,13 @@
 package com.example.emprego.model;
 
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -36,5 +38,8 @@ public class Funcionario {
 
     @Column(nullable = false, unique = true)
     private String email;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Endereco endereco;
  
 }
