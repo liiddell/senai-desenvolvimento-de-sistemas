05e8e501f01f5de0b76d1e57acd23176fc729b2b
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-11-17 11:14:02 -0300

    Finalização

--- react.js/consumo-de-api/src/App.jsx
+++ react.js/consumo-de-api/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+
 import Header from './components/Header'
 import Footer from './components/Footer'
-import { Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import CadastroPage from './pages/CadastroPage'
 import ListaPage from './pages/ListaPage'
+import { ToastContainer } from 'react-toastify'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -21,6 +23,7 @@ function App() {
         </Routes>
       </main>
       <Footer/>
+      <ToastContainer position='top-right' autoClose={3000} />
     </>
   )
 }
