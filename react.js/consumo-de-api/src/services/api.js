c2ef95611ccf87a25571530e77fdd5bd4b178bc0
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-11-18 10:26:04 -0300

    Conexão backend e frontend

--- react.js/consumo-de-api/src/services/api.js
+++ react.js/consumo-de-api/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "https://localhost:8080",
+  baseURL: "http://localhost:8080",
   timeout: 5000,
 });
 
--- spring-boot/consumoDeApi/src/main/java/com/example/consumoDeApi/controller/UsuarioController.java
+++ spring-boot/consumoDeApi/src/main/java/com/example/consumoDeApi/controller/UsuarioController.java
@@ -13,6 +13,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/usuarios")
+@CrossOrigin(origins = "http://localhost:5173")
 public class UsuarioController {
     @Autowired
     private UsuarioServices usuarioServices;
