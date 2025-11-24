80e48e397a4f1913614d91557bfadab885100e73
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-11-24 08:30:52 -0300

    Criptografia

--- spring-boot/consumoDeApi/src/main/java/com/example/consumoDeApi/services/UsuarioServices.java
+++ spring-boot/consumoDeApi/src/main/java/com/example/consumoDeApi/services/UsuarioServices.java
@@ -6,6 +6,7 @@ import com.example.consumoDeApi.model.UsuarioModel;
 import com.example.consumoDeApi.repository.UsuarioRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,6 +15,10 @@ import java.util.List;
 public class UsuarioServices {
     @Autowired
     private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
 
     public List<UsuarioResponseDTO> listar() {
         return usuarioRepository.findAll()
@@ -30,7 +35,7 @@ public class UsuarioServices {
         UsuarioModel usuario = new UsuarioModel();
         usuario.setNome(dto.getNome());
         usuario.setEmail(dto.getEmail());
-        usuario.setSenha(dto.getSenha());
+        usuario.setSenha(passwordEncoder.encode(dto.getSenha()));
 
         usuarioRepository.save(usuario);
         return usuario;
